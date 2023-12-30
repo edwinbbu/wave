c31abbcd67d968fc5fc3e75e43db2443a7cf78cc
edwinbbu <edwinbbu@gmail.com>
2023-12-30 12:24:18 +0530

    Tiptap editor added

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Tiptap } from "./Tiptap";
 
 export const App = () => {
   return (
-    <div className="text-5xl text-blue-600">
-      React with ts, vite and tailwindcss.
+    <div className="px-6 container mx-auto">
+      <div className="text-5xl text-gray-700 pb-4">Wave</div>
+      <div className="border border-gray-200 p-4">
+        <Tiptap />
+      </div>
     </div>
   );
 };
