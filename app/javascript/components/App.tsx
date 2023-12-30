612bf6d73c15439dc23c2bb7536c1f85eab70d88
edwinbbu <edwinbbu@gmail.com>
2023-12-31 01:05:59 +0530

    Postcss support added and removed build files

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -3,7 +3,7 @@ import { Tiptap } from "./Tiptap";
 
 export const App = () => {
   return (
-    <div className="px-6 container mx-auto">
+    <div className="p-6 container mx-auto">
       <div className="text-5xl text-gray-700 pb-4">Wave</div>
       <div className="border border-gray-200 p-4">
         <Tiptap />
